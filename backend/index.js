64517a6863d297c918e7457137e7ab42e5e74b82
van an <van.an@boxyz.com>
2022-12-14 21:14:21 +0700

    Remove code not use

--- backend/index.js
+++ backend/index.js
@@ -7,28 +7,19 @@ import connectDB from './config/db/db.js'
 import ProductRouter from './routers/ProductRouter.js'
 import UserRouter from './routers/UserRouter.js'
 import OrderRouter from './routers/OrderRouter.js'
-import ChatRouter from './routers/ChatRouter.js'
 
 import {createServer} from 'http'
-// import {Server} from 'socket.io'
-
-import {ConnectSocket} from './config/socket/socket.js'
 
 import cloudinary from './config/cloudinary/cloudinary.js'
-import PaymentRouter from './routers/PaymentRouter.js'
-import SelectListrouter from './routers/SelectListRouter.js'
-import ListTypeProductRouter from './routers/ListTypeProductRouter.js'
 import CategoryRouter from './routers/CategoryRouter.js'
 import DashboardRouter from './routers/DashboardRouter.js'
 
 dotenv.config();
-process.env.TOKEN_SECRET;
 
 const app = express()
 const PORT = process.env.PORT || 4000
 const server = createServer(app)
 
-ConnectSocket(server)
 connectDB()
 
 app.use(cors())
@@ -39,15 +30,7 @@ app.use('/category', CategoryRouter)
 app.use('/products', ProductRouter)
 app.use('/user', UserRouter)
 app.use('/order', OrderRouter)
-app.use('/chat', ChatRouter)
-app.use('/payment', PaymentRouter)
-app.use('/selectList', SelectListrouter)
-app.use('/typeList', ListTypeProductRouter)
 app.use('/dashboard', DashboardRouter)
-
-app.get('/api/config/paypal', (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
-})
 
 app.post('/api/upload', async (req, res) => {
     try {
@@ -57,8 +40,7 @@ app.post('/api/upload', async (req, res) => {
         });
         res.json({ msg: 'yaya' });
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ err: 'Something went wrong' });
+        res.status(500).json({ err: 'Lỗi ' + err });
     }
 });
 
